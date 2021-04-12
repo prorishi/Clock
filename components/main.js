5e244649d6d23e81d88331a9709064512923c2ee
ProRishi <3rishi11@gmail.com>
2021-04-12 12:49:41 +0530

    fnal-v1

--- components/main.js
+++ components/main.js
@@ -1,3 +1,4 @@
+// Reference Elements from the DOM.
 const hands = [
     document.getElementById("hour"), 
     document.getElementById("minute"), 
@@ -5,24 +6,28 @@ const hands = [
 ];
 const digitalClock = document.getElementById("digital");
 
-setInterval(main, 100);
+// Execute the clock function every 100ms, to sustain the clocks.
+setInterval(clock, 100);
 
+// Rotates an element by a specified angle.
 function rotate(element, angle) {
     element.style.transform = `rotateZ(${angle + 180}deg)`;
 }
 
-function lt10(number) {
+// Prefixes the number with a zero if it's not two-digit; needed for the digital clock.
+function pad(number) {
     if (number < 10) return '0' + number
     return number
 }
 
-function main() {
+// Handles both the Clocks.
+function clock() {
     let time = new Date();
     let hours = time.getHours(),
         minutes = time.getMinutes(),
         seconds = time.getSeconds();
 
-    digitalClock.innerText = `${lt10(hours)}:${lt10(minutes)}:${lt10(seconds)}`;
+    digitalClock.innerText = `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
 
     if (hours > 12) hours -= 12;
     if (hours == 0) hours = 12;
---
+++ service-worker.js
@@ -0,0 +1,36 @@
+// Name of the Cache.
+const CACHE = "cacheV1";
+
+// Select files for caching.
+let urlsToCache = [
+    "./",
+    "./index.html",
+    "./components", 
+    "./components/favicon.png", 
+    "./components/clock-face.png",
+    "./components/main.css", 
+    "./components/main.js", 
+    "./components/pwa-handler.js"
+];
+
+// Cache all the selected items once application is installed.
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE).then((cache) => {
+            console.log("Caching started.");
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Whenever a resource is requested, return if its cached else fetch the resourcefrom server.
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
