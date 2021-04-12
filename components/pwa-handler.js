dce946627db1316d004caaf44cf3d09ad4f502d7
ProRishi <3rishi11@gmail.com>
2021-04-12 17:43:54 +0530

    he

--- components/pwa-handler.js
+++ components/pwa-handler.js
@@ -6,7 +6,7 @@ if (serviceWorker) {
     serviceWorker
         .register("service-worker.js")
         .then(() => console.log("ServiceWorker Registered to the Application!"))
-        .catch(() => console.log("Failed to Register the ServiceWorker."));
+        .catch((f) => console.log("Failed to Register the ServiceWorker.",f));
 }
 
 // Create a variable to defer the beforeinstallprompt event.
